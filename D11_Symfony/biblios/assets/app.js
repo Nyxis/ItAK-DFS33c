b7f188c705c99f834012bac36cd15695108cc78f
BaptFr <baptistesalazar@gmail.com>
2025-06-18 12:26:04 +0200

    Soucis Postgres et env réglés - Création schéma base de données x4 entités + relations

---
+++ D11_Symfony/biblios/assets/app.js
@@ -0,0 +1,7 @@
+import './bootstrap.js';
+import './js/scripts.js';
+import './vendor/bootstrap/dist/css/bootstrap.min.css';
+import './styles/styles.css';
+import './styles/app.css';
+
+console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
